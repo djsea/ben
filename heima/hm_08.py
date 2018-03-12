09ed6b4d6bcfb972aeabbfed2e060d413ff3fbb8
dujun <djsea@139.com>
2018-03-12 14:28:23 +0800

    九九乘法表

---
+++ heima/hm_08.py
@@ -0,0 +1,15 @@
+row = 1
+
+while row <= 9:
+
+    col = 1
+
+    while col <= row:
+
+        #print("*", end="")
+        print(" %d * %d = %d" % (row, col, row * col), end="  ")
+        col += 1
+
+    print("")
+
+    row += 1
